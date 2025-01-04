1f0f1e59876143ec71e6d061694ecfb1028d025b
pixel nebula <pixelnebulacontact@gmail.com>
2025-01-04 11:36:58 -0800

    Initial commit

--- Games/Littlealchemy2/patch/js/poki-master-loader.js
+++ Games/Littlealchemy2/patch/js/poki-master-loader.js
@@ -1,15 +1,15 @@
 "use strict";
 var scripts = document.getElementsByTagName("script")
   , scriptUrl = scripts[scripts.length - 1].src
-  , root = scriptUrl.split("master-loader.js")[0]
+  , root = scriptUrl.split("https://cdn.rawgit.org/pixel-nebula/pixel-nebula.github.io/master/Games/Littlealchemy2/master-loader.js")[0]
   , loaders = {
-    unity: "unity.js",
-    "unity-beta": "unity-beta.js",
-    "unity-2020": "unity-2020.js"
+    unity: "https://cdn.rawgit.org/pixel-nebula/pixel-nebula.github.io/master/Games/Littlealchemy2/unity.js",
+    "unity-beta": "https://cdn.rawgit.org/pixel-nebula/pixel-nebula.github.io/master/Games/Littlealchemy2/unity-beta.js",
+    "unity-2020": "https://cdn.rawgit.org/pixel-nebula/pixel-nebula.github.io/master/Games/Littlealchemy2/unity-2020.js"
 };
-if (0 <= window.location.href.indexOf("pokiForceLocalLoader") && (loaders.unity = "/unity/dist/unity.js",
-loaders["unity-beta"] = "/unity-beta/dist/unity-beta.js",
-loaders["unity-2020"] = "/unity-2020/dist/unity-2020.js",
+if (0 <= window.location.href.indexOf("pokiForceLocalLoader") && (loaders.unity = "https://cdn.rawgit.org/pixel-nebula/pixel-nebula.github.io/master/Games/Littlealchemy2//unity/dist/unity.js",
+loaders["unity-beta"] = "https://cdn.rawgit.org/pixel-nebula/pixel-nebula.github.io/master/Games/Littlealchemy2//unity-beta/dist/unity-beta.js",
+loaders["unity-2020"] = "https://cdn.rawgit.org/pixel-nebula/pixel-nebula.github.io/master/Games/Littlealchemy2//unity-2020/dist/unity-2020.js",
 root = "/loaders"),
 !window.config)
     throw Error("window.config not found");
