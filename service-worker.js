832176793c824f22305d253d2fca41628ef56c73
pixel nebula <pixelnebulacontact@gmail.com>
2025-01-06 18:07:50 -0800

    Initial commit

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,7 @@ const URLS_TO_CACHE = [
   'Games.html', // Cache Games.html
   'Home.html', // Cache Home.html
   'https://cdn.jsdelivr.net/gh/pixel-nebula/pixel-nebula.github.io@master/particles.js', // Cache particles.js (assuming it's a JS file)
+  'https://cdn.jsdelivr.net/gh/pixel-nebula/pixel-nebula.github.io@master/Lasar.js', // Cache particles.js (assuming it's a JS file)
   'Settings.html', // Cache Settings.html
   'termsofservice.html', // Cache terms of service HTML
   'https://cdn.jsdelivr.net/gh/pixel-nebula/pixel-nebula.github.io@master/Pictures/Logo.png',
